cf16b0704eeaaa407fa63d41c48fe370a373f268
Akram Masmoudi <masmoudiakram14@gmail.com>
2020-08-23 23:13:41 +0100

    cheque

--- src/app/echeance-des-cheques/echeance-des-cheques.component.ts
+++ src/app/echeance-des-cheques/echeance-des-cheques.component.ts
@@ -11,5 +11,16 @@ export class EcheanceDesChequesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  today = new Date();
+  total = 0;
+  gridWith = "width: 100%; height: calc(100vh - 125px);";
+  columnDefs = [
+		{headerName: 'Cheque Number', field: 'chequeNumber',editable : true,sortable: true, filter: true,resizable: true,width : 160 },
+		{headerName: 'Banque', field: 'banque',editable : true,sortable: true, filter: true,resizable: true,flex : 1},
+		{headerName: 'Agence', field: 'agence',editable : true,sortable: true, filter: true,resizable: true,flex : 1},
+		{headerName: 'Date', field: 'date',editable : true,sortable: true, filter: true,resizable: true,width : 140},
+		{headerName: 'Amount', field: 'amount',editable : true,sortable: true, filter: true,resizable: true,width : 140},
+	];
 
+	rowData = [];
 }
